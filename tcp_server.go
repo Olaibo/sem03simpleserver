13679ac7126256b167cf9897391ecd031390b70c
Olaibo <olai.bergh@gmail.com>
2023-04-24 08:09:43 +0000

    oppdatert tcp_server.go

--- tcp_server.go
+++ tcp_server.go
@@ -1,52 +1,68 @@
 package main
 
-import (
+import(
 	"io"
 	"log"
 	"net"
 	"sync"
+	"github.com/Olaibo/is105sem03/mycrypt"
+	"github.com/Trym123/minyr/yr"
 )
 
 func main() {
 
-	var wg sync.WaitGroup
+	varwgsync.WaitGroup
 
-	server, err := net.Listen("tcp", "172.17.0.3:38527")
-	if err != nil {
+	server, err:=net.Listen("tcp", "172.17.0.3:38527")
+	iferr!=nil{
 		log.Fatal(err)
 	}
 	log.Printf("bundet til %s", server.Addr().String())
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
+	gofunc() {
+		deferwg.Done()
+		for{
 			log.Println("før server.Accept() kallet")
-			conn, err := server.Accept()
-			if err != nil {
+			conn, err:=server.Accept()
+			iferr!=nil{
 				return
 			}
-			go func(c net.Conn) {
-				defer c.Close()
-				for {
-					buf := make([]byte, 1024)
-					n, err := c.Read(buf)
-					if err != nil {
-						if err != io.EOF {
+			gofunc(cnet.Conn) {
+				deferc.Close()
+				for{
+					buf:=make([]byte, 1024)
+					n, err:=c.Read(buf)
+					iferr!=nil{
+						iferr!=io.EOF{
 							log.Println(err)
 						}
-						return // fra for løkke
+						return// fra for løkke
 					}
-					switch msg := string(buf[:n]); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
+
+					dekryptertMelding:=mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					log.Println("Dekrypter melding: ", string(dekryptertMelding))
+					switchmsg:=string(dekryptertMelding); msg{
+  				        case"ping":
+						//_, err = c.Write([]byte("pong"))
+						kryptertMelding:=mycrypt.Krypter([]rune(string("pong")), mycrypt.ALF_SEM03, 4)
+						log.Println("krypter melding: ", string(kryptertMelding))
+						_, err=conn.Write([]byte(string(kryptertMelding)))
+					case"Kjevik;SN39040;18.03.2022 01:50;6":
+						newString, err:=yr.CelsiusToFahrenheitLine("Kjevik;SN39040;18.03.2022 01:50;6")
+							iferr!=nil{
+							log.Fatal(err)
+							}
+							kryptertMelding:=mycrypt.Krypter([]rune(string(newString)), mycrypt.ALF_SEM03, 4)
+							_, err=conn.Write([]byte(string(kryptertMelding)))
+
 					default:
-						_, err = c.Write(buf[:n])
+						_, err=c.Write(buf[:n])
 					}
-					if err != nil {
-						if err != io.EOF {
+					iferr!=nil{
+						iferr!=io.EOF{
 							log.Println(err)
 						}
-						return // fra for løkke
+						return// fra for løkke
 					}
 				}
 			}(conn)
